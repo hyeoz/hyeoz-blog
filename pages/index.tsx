34acdf4cbe3280676e00d7ee493c269632160e61
hyeoz <jenny970203@gmail.com>
2023-05-01 16:17:22 +0900

    chap. 8 data fetching UI prepare

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
+import Title from "@/components/Title";
 import Head from "next/head";
 import React from "react";
 
+const products = [
+  { id: 1, title: "First Product" },
+  { id: 2, title: "Second Product" },
+];
+
 export default function HomePage(): React.ReactElement {
+  console.log(products);
+
   return (
     <>
       <Head>Next Shop</Head>
       <main className="px-6 py-4">
-        <h1 className="pb-4 text-2xl">next shop</h1>
-        <p>[TODO : display products]</p>
+        <Title>Next Shop</Title>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+        </ul>
       </main>
     </>
   );
