74a797a1abc693b88dd83e79bba05915a9223550
hyeoz <jenny970203@gmail.com>
2023-08-03 00:11:41 +0900

    add: 헤더 레이아웃

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,36 +1,15 @@
 import Link from "next/link";
 import ThemeSwitch from "./ThemeSwitch";
+import dayjs from "dayjs";
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href={"/"}>Home</Link>
-        </li>
-        <li>
-          <Link href={"/about"}>About</Link>
-        </li>
-      </ul>
-      <ThemeSwitch />
-      <style jsx>
-        {`
-          nav {
-            display: flex;
-            justify-content: space-between;
-          }
-          ul {
-            list-style-type: none;
-            padding: 0;
-          }
-          li {
-            display: inline;
-          }
-          li:not(:first-of-type) {
-            margin-left: 0.75rem;
-          }
-        `}
-      </style>
+    <nav className="main-container mt-6 mb-4 p-4 border grid grid-cols-5 gap-2">
+      <div className="col-span-1 text-center">
+        <h2 className="text-4xl font-bold">{dayjs().format("YY.MM.DD")}</h2>
+        <p className="text-lg">{true ? "오늘은 경기가 없어요!" : ""}</p>
+      </div>
+      <div className="col-span-4 border">BANNER AREA</div>
     </nav>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,16 @@ import Link from "next/link";
 
 function HomePage(props) {
   console.log("=====> HOME PAGE", props);
-  const { posts } = props;
+  // const { posts } = props;
   return (
     <>
       <Head>
-        <title>NEXT BLOG</title>
+        <title>HYEOZ</title>
         <meta name="description" defaultValue="This is my blog" />
       </Head>
       <main>
-        <h1>My Blog</h1>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.title}>
-              <Link href={`/post/${post.path}`}>{post.title}</Link>
-            </li>
-          ))}
-        </ul>
+        <h1>CONTENT</h1>
+        <ul></ul>
       </main>
     </>
   );
